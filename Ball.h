ef2d1ffebbef9c90b265cf1a08660da500193cf7
Mustafin Maksim <74867408+MustafinMP@users.noreply.github.com>
2026-03-16 15:51:44 +0500

    Add files via upload

---
+++ Ball.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class Ball { 
+private:
+  int x;
+  int y;
+  int radius;
+  int vx;
+  int vy;
+  sf::CircleShape circle;
+
+public:
+  Ball(int x, int y, int radius);
+  void move(const int width, const int height);
+  void pong();
+  void draw(sf::RenderWindow& window);
+};
+
