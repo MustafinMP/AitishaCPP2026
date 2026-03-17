9d726b5a6c8bd38e8054b4a1637351850601021b
Mustafin Maksim <74867408+MustafinMP@users.noreply.github.com>
2026-03-17 08:28:55 +0500

    Update SFMLApplication2.cpp

--- PingPong/SFMLApplication2.cpp
+++ PingPong/SFMLApplication2.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include "Ball.h"
+#include "Racket.h"
 
 int main() {
   const int width = 1000;
@@ -8,6 +9,9 @@ int main() {
   sf::RenderWindow window(sf::VideoMode({ width, height }), "Ping Pong");
   window.setFramerateLimit(60);
   Ball ball(500, 300, 30);
+  Racket leftRacket(50, 150);
+  Racket rightRacket(900, 150);
+
   
   while (window.isOpen()) {
     while (const std::optional event = window.pollEvent()) {
@@ -18,6 +22,8 @@ int main() {
     ball.move(width, height);
 
     window.clear();
+    leftRacket.draw(window);
+    rightRacket.draw(window);
     ball.draw(window);
     window.display();
   }
