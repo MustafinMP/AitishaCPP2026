c240e849424f22a8e0bf864564083dbb4a1e24e1
Mustafin Maksim <74867408+MustafinMP@users.noreply.github.com>
2026-05-18 21:59:40 +0500

    Update tetrisSFML.cpp

--- tetrisSFML.cpp
+++ tetrisSFML.cpp
@@ -1,17 +1,84 @@
-﻿#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
 #include <string>
 #include <ctime>
 
+#include <array>
+#include <cctype>
+#include <stdexcept>
+
 using std::vector;
 using std::abs;
 using std::string;
+using std::array;
 using sf::Color;
 
 const int width = 10;
 const int height = 20;
 const int scale = 40;
+
+using ShapeMask = array<array<bool, 4>, 4>;
+using ShapeMasks = array<ShapeMask, 4>;
+
+const ShapeMasks MASK_I = { {
+    {{{{0,0,0,0}}, {{1,1,1,1}}, {{0,0,0,0}}, {{0,0,0,0}}}},
+    {{{{0,1,0,0}}, {{0,1,0,0}}, {{0,1,0,0}}, {{0,1,0,0}}}},
+    {{{{0,0,0,0}}, {{1,1,1,1}}, {{0,0,0,0}}, {{0,0,0,0}}}},
+    {{{{0,1,0,0}}, {{0,1,0,0}}, {{0,1,0,0}}, {{0,1,0,0}}}}
+} };
+
+const ShapeMasks MASK_O = { {
+    {{{{0,0,0,0}}, {{0,1,1,0}}, {{0,1,1,0}}, {{0,0,0,0}}}},
+    {{{{0,0,0,0}}, {{0,1,1,0}}, {{0,1,1,0}}, {{0,0,0,0}}}},
+    {{{{0,0,0,0}}, {{0,1,1,0}}, {{0,1,1,0}}, {{0,0,0,0}}}},
+    {{{{0,0,0,0}}, {{0,1,1,0}}, {{0,1,1,0}}, {{0,0,0,0}}}}
+} };
+
+const ShapeMasks MASK_T = { {
+    {{{{0,1,0,0}}, {{1,1,1,0}}, {{0,0,0,0}}, {{0,0,0,0}}}},
+    {{{{0,1,0,0}}, {{0,1,1,0}}, {{0,1,0,0}}, {{0,0,0,0}}}},
+    {{{{0,0,0,0}}, {{1,1,1,0}}, {{0,1,0,0}}, {{0,0,0,0}}}},
+    {{{{0,1,0,0}}, {{1,1,0,0}}, {{0,1,0,0}}, {{0,0,0,0}}}}
+} };
+
+const ShapeMasks MASK_S = { {
+    {{{{0,1,1,0}}, {{1,1,0,0}}, {{0,0,0,0}}, {{0,0,0,0}}}},
+    {{{{0,1,0,0}}, {{0,1,1,0}}, {{0,0,1,0}}, {{0,0,0,0}}}},
+    {{{{0,1,1,0}}, {{1,1,0,0}}, {{0,0,0,0}}, {{0,0,0,0}}}},
+    {{{{0,1,0,0}}, {{0,1,1,0}}, {{0,0,1,0}}, {{0,0,0,0}}}}
+} };
+
+const ShapeMasks MASK_Z = { {
+    {{{{1,1,0,0}}, {{0,1,1,0}}, {{0,0,0,0}}, {{0,0,0,0}}}},
+    {{{{0,0,1,0}}, {{0,1,1,0}}, {{0,1,0,0}}, {{0,0,0,0}}}},
+    {{{{1,1,0,0}}, {{0,1,1,0}}, {{0,0,0,0}}, {{0,0,0,0}}}},
+    {{{{0,0,1,0}}, {{0,1,1,0}}, {{0,1,0,0}}, {{0,0,0,0}}}}
+} };
+
+const ShapeMasks MASK_J = { {
+    {{{{0,1,0,0}}, {{0,1,0,0}}, {{1,1,0,0}}, {{0,0,0,0}}}},
+    {{{{0,0,0,0}}, {{1,1,1,0}}, {{1,0,0,0}}, {{0,0,0,0}}}},
+    {{{{0,0,0,0}}, {{1,1,0,0}}, {{0,1,0,0}}, {{0,1,0,0}}}},
+    {{{{0,0,0,0}}, {{0,0,1,0}}, {{1,1,1,0}}, {{0,0,0,0}}}}
+} };
+
+const ShapeMasks MASK_L = { {
+    {{{{0,0,1,0}}, {{0,0,1,0}}, {{0,1,1,0}}, {{0,0,0,0}}}},
+    {{{{0,0,0,0}}, {{1,1,1,0}}, {{0,0,1,0}}, {{0,0,0,0}}}},
+    {{{{0,0,0,0}}, {{0,1,1,0}}, {{0,1,0,0}}, {{0,1,0,0}}}},
+    {{{{0,0,0,0}}, {{1,0,0,0}}, {{1,1,1,0}}, {{0,0,0,0}}}}
+} };
+
+ShapeMasks getShapeMask(char type) {
+  if (type == 'I') { return MASK_I; }
+  if (type == 'J') { return MASK_J; }
+  if (type == 'L') { return MASK_L; }
+  if (type == 'O') { return MASK_O; }
+  if (type == 'Z') { return MASK_Z; }
+  if (type == 'T') { return MASK_T; }
+  if (type == 'S') { return MASK_S; }
+}
 
 vector<char> shapeTypes = { 'I', 'J', 'L', 'O', 'Z', 'T', 'S' };
 
@@ -33,9 +100,58 @@ class Shape {
 public:
   int x, y, rotation, colorId;
   char type;
+  ShapeMasks masks;
 
-  Shape() : type(randomShapeType()), x(3), y(0), rotation(0), colorId(randomColor()) {};
-  Shape(char type, int x, int y, int rotation, int colorId) : type(type), x(x), y(y), rotation(rotation), colorId(colorId) {};
+  Shape() : type(randomShapeType()), x(3), y(0), rotation(0), colorId(randomColor()) {
+    this->masks = getShapeMask(this->type);
+  };
+  Shape(char type, int x, int y, int rotation, int colorId) : type(type), x(x), y(y), rotation(rotation), colorId(colorId) {
+    this->masks = getShapeMask(this->type);
+  };
+  void move() {
+    y += 1;
+  };
+  void rotate() {};
+
+  bool onBottom() {
+    for (int sy = 0; sy < 4; sy++) {
+      for (int sx = 0; sx < 4; sx++) {
+        if (masks[rotation][sy][sx] and y + sy == height - 1) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  void addToField(vector<vector<int>>& field) {
+    for (int sy = 0; sy < 4; sy++) {
+      for (int sx = 0; sx < 4; sx++) {
+        if (masks[rotation][sy][sx]) {
+          field[y + sy][x + sx] = colorId;
+        }
+      }
+    }
+  }
+
+  void draw(sf::RenderWindow& window) {
+    for (int sy = 0; sy < 4; sy++) {
+      for (int sx = 0; sx < 4; sx++) {
+        sf::RectangleShape rect;
+        rect.setSize(sf::Vector2f(scale, scale));
+        rect.setPosition(sf::Vector2f((sx + x) * scale, (sy + y) * scale));
+        if (masks[rotation][sy][sx]) {
+          rect.setFillColor(getColor(colorId));
+        }
+        else {
+          rect.setFillColor(Color::Black);
+        }
+        rect.setOutlineColor(Color::White);
+        rect.setOutlineThickness(1);
+        window.draw(rect);
+      }
+    }
+  }
 };
 
 
@@ -58,18 +174,35 @@ public:
   };
 
   void handleKeyboard() {};
-  bool move() {};
+
+  bool move() {
+    currentShape.move();
+    if (currentShape.onBottom()) {
+      currentShape.addToField(field);
+      checkRows();
+      createNewShape();
+      return true;
+    }
+    return false;
+  };
+
   void rotate() {};
   bool collided() {};
   bool isFinished() {};
   void checkRows() {};
+
   void draw(sf::RenderWindow& window) {
     for (int y = 0; y < height; y++) {
       for (int x = 0; x < width; x++) {
         sf::RectangleShape rect;
         rect.setSize(sf::Vector2f(scale, scale));
         rect.setPosition(sf::Vector2f(x * scale, y * scale));
-        rect.setFillColor(Color::Black);
+        if (field[y][x] == 0) {
+          rect.setFillColor(Color::Black);
+        }
+        else {
+          rect.setFillColor(getColor(field[y][x]));
+        }
         rect.setOutlineColor(Color::White);
         rect.setOutlineThickness(1);
         window.draw(rect);
@@ -80,11 +213,14 @@ public:
     rect.setPosition(sf::Vector2f(width * scale, 0));
     rect.setFillColor(Color::White);
     window.draw(rect);
+
+    currentShape.draw(window);
   };
 };
 
 int main() {
   srand(time(nullptr));
+  bool touched = false;
   sf::RenderWindow window(sf::VideoMode({ (width + 6) * scale, height * scale }), "Game");
   window.setFramerateLimit(1);
 
@@ -97,6 +233,8 @@ int main() {
 
       if (const auto* keypress = event->getIf<sf::Event::KeyPressed>()) {}
     };
+    touched = tetris.move();
+
     window.clear();
     tetris.draw(window);
     window.display();
